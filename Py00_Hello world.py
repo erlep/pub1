87e863c6a3aa7a0b03d8625b2d54d2fda3cfc1c2
erlep <egix@centrum.cz>
2022-01-06 10:56:43 +0100

    v02

---
+++ Py00_Hello world.py
@@ -0,0 +1,30 @@
+﻿#!/usr/bin/env python3
+
+# import sys
+
+'''
+This is Python 1st Hello world script
+
+## made by peg - https://GitHub.com/ErleP
+
+'''
+print(__doc__)
+
+# Python soubor
+
+# %% Odkaz 1 - umoznuje Run Cell Bellow
+print("Hello world")
+
+# %% Odkaz 2 - umoznuje Run Cell Bellow
+print('OkDone.')
+
+# %% string
+s = 'Hi\nHello'
+print(s)
+
+# %% Python Raw String
+raw_s = r'Hi\nHello'
+print(raw_s)
+
+# The End
+# sys.exit()
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # pub1
+
 test public repository
+
+## v02
+
+06.01.2022 10:50 - verze z vs code
